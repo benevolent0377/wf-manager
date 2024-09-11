2bc8f6f77b4aa7b8d7e41164c6cf6248812c4981
calithos4136 <149543612+calithos4136@users.noreply.github.com>
2024-09-10 22:32:49 -0500

    added code snippets to sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,17 @@
-API_KEY = "openapi.json"
+import requests
+
+
+StatAPIHost = "https://api.warframestat.us/"
+WikiAPIHost = "https://wf.snekw.com/"
+
+response = requests.get(StatAPIHost + "drops/search/oxium")
+
+if response.status_code == 200:
+
+    data = response.json()
+    print(data)
+    # print(((data['data'])['Mods'])['Necramech Vitality'])
+    print("success")
+
+else:
+    print("bummer")
