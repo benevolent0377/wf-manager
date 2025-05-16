b6963d9c0c7474abeebfcdc8887aa32a771a2e31
benevolent0377 <149543612+benevolent0377@users.noreply.github.com>
2025-05-16 18:57:36 +0000

    created main.py to host the main code of the program on startup

---
+++ source/lib/main.py
@@ -0,0 +1,10 @@
+from source.lib import filesys, GUI, api
+from source.core.system import quitKill
+
+def run():
+
+    FileSystem = filesys.FileSystem(['config', 'cache', 'tmp', 'log', '.userdata', '.pgmdata'])
+    FileSystem.buildConfig(['local.yaml'], [])
+
+    quitKill()
+    # GUI.main()
