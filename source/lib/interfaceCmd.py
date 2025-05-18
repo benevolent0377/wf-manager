907f064f622eb019afe77633a698fbd02d2b5799
calithos4136 <149543612+calithos4136@users.noreply.github.com>
2025-05-18 00:07:54 -0500

    made changes

--- source/lib/interfaceCmd.py
+++ source/lib/interfaceCmd.py
@@ -1,1 +1,23 @@
 #a file for the GUI commands
+
+global app
+
+def init(item):
+    global app
+
+    app = item
+
+def viewSelection(item):
+    print(item)
+
+def menuSelection(item):
+    print(item)
+
+def accountSelection(item):
+    print(item)
+
+def search(event=""):
+    print(app.mainFrame.searchBox.get())
+
+def autoComplete(item):
+    pass
